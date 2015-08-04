245e48e198cf22d406b373b76b6cfe1e27d6831c
Zeina Dajani <dajani.zeina@gmail.com>
2015-08-04 16:05:00 +0100

    added first and last name to student

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  
+  protected 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+
+  def sign_up_params
+    params.require(:student).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   it { is_expected.to have_many :pairs }
+  it { is_expected.to have_many :assigned_pairs }
 end
