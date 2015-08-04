f17ff4e24d6e80992d2c3d7b3fea7393c8cc29ff
Zeina Dajani <dajani.zeina@gmail.com>
2015-08-04 17:23:26 +0100

    fixed student model for last and first name

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+ 
   has_many :pairs
   has_many :blacklisted_students
   has_many :assigned_pairs, through: :pairs, source: :student
