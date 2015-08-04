7f53fd6c4d4e33037bc972854cee3d53f13e43f3
Zeina Dajani <dajani.zeina@gmail.com>
2015-08-04 15:18:38 +0100

    fixed rspec

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -1,1 +1,5 @@
-###
+require 'rails_helper'
+
+RSpec.describe Pair, type: :model do
+  it { should belong_to(:student) }
+end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,1 +1,5 @@
-####
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+  it { is_expected.to have_many :pairs }
+end
