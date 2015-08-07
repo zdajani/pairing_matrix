657d15ef257a7dd1c34aad828cc1683ee134c97a
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-08-07 11:32:17 +0100

    add dropbox, fix tests tbc

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,14 +1,15 @@
-class PairingsController < ApplicationController  
+class PairingsController < ApplicationController
   before_action :authenticate_student!, :except => [:index]
   def index
     @students = Student.all
     @pairs = Pair.all
+    @pairs_length = @students.map { |student| [student.id] }
+    # @pairs_length = (Student.all.length - 1).to_s
   end
-  
-  def create
 
-      current_student.output_day_pairs(Student.all, params[:pairing_session].to_i)
-    redirect_to root_path
+  def create
+    current_student.output_day_pairs(Student.all, params[:pairing_session].to_i)
+  redirect_to root_path
   end
-  
+
 end
